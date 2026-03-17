d588eaa11569a8b04a3e1f282fcdb4ce5e567fb8
PiccioHub <picciohub@gmail.com>
2026-03-17 12:57:17 +0100

    Changed from JSON to intended json to better read the JSONs

--- internal/server/utils.go
+++ internal/server/utils.go
@@ -5,11 +5,17 @@ import (
 	"net/http"
 )
 
+type envelop map[string]interface{}
+
+type UserKeyString string
+
+const UserKey = UserKeyString("userKey")
+
 type Response struct {
-	Success bool        `json:"success"`
-	Data    interface{} `json:"data,omitempty"`
-	Error   string      `json:"error,omitempty"`
-	Meta    *Meta       `json:"meta,omitempty"`
+	Success bool    `json:"success"`
+	Data    envelop `json:"data,omitempty"`
+	Error   string  `json:"error,omitempty"`
+	Meta    *Meta   `json:"meta,omitempty"`
 }
 
 type Meta struct {
@@ -19,35 +25,35 @@ type Meta struct {
 	TotalPages int `json:"total_pages,omitempty"`
 }
 
-func (s *Server) Ok(c *gin.Context, data interface{}, meta *Meta) {
-	c.JSON(http.StatusOK, Response{
+func (s *Server) Ok(c *gin.Context, data envelop, meta *Meta) {
+	c.IndentedJSON(http.StatusOK, Response{
 		Success: true,
 		Data:    data,
 		Meta:    meta,
 	})
 }
 
-func (s *Server) Created(c *gin.Context, data interface{}) {
-	c.JSON(http.StatusCreated, Response{
+func (s *Server) Created(c *gin.Context, data envelop) {
+	c.IndentedJSON(http.StatusCreated, Response{
 		Success: true,
 		Data:    data,
 	})
 }
 
 func (s *Server) Updated(c *gin.Context) {
-	c.JSON(http.StatusNoContent, Response{
+	c.IndentedJSON(http.StatusNoContent, Response{
 		Success: true,
 	})
 }
 
 func (s *Server) Delete(c *gin.Context) {
-	c.JSON(http.StatusNoContent, Response{
+	c.IndentedJSON(http.StatusNoContent, Response{
 		Success: true,
 	})
 }
 
 func (s *Server) Fail(c *gin.Context, status int, message string) {
-	c.JSON(status, Response{
+	c.IndentedJSON(status, Response{
 		Success: false,
 		Error:   message,
 	})
