6a4220f664d28199f450682767faeec5154823f2
PiccioHub <picciohub@gmail.com>
2026-03-17 12:53:32 +0100

    Removed useless role/users table, moved the role auth to the custom claims of tokens ( no db query needed )

--- migrations/20260316160453_createbasictables.sql
+++ migrations/20260316160453_createbasictables.sql
@@ -1,19 +1,4 @@
 -- +goose Up
-CREATE TABLE user_roles (
-                            ID SERIAL NOT NULL PRIMARY KEY,
-                            role VARCHAR(255) NOT NULL
-);
-
-INSERT INTO user_roles(role) VALUES ('editor');
-
-CREATE TABLE users (
-                       UID VARCHAR(255) NOT NULL PRIMARY KEY,
-                       role_id INT NOT NULL,
-
-                       CONSTRAINT role_fk
-                           FOREIGN KEY (role_id) REFERENCES user_roles(ID) ON DELETE CASCADE
-);
-
 CREATE TABLE sections (
                           ID SERIAL NOT NULL PRIMARY KEY,
                           Nome VARCHAR(255) NOT NULL ,
