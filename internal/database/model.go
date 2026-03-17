ea1e6632e4187ac11b47c7c4c43b4613788d6311
PiccioHub <picciohub@gmail.com>
2026-03-17 12:54:54 +0100

    removed also the implementation of the two tables

--- internal/database/model.go
+++ internal/database/model.go
@@ -3,11 +3,8 @@ package database
 import "github.com/jackc/pgx/v5/pgxpool"
 
 type Models struct {
-	UsersModel UsersModel
 }
 
 func NewModels(DB *pgxpool.Pool) Models {
-	return Models{
-		UsersModel: UsersModel{DB: DB},
-	}
+	return Models{}
 }
